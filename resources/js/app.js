848d18cd73b85701ca087cc1bf61ae86675f9afb
Yurii Miroshnyk <lemadioro@gmail.com>
2024-08-09 17:27:16 +0300

    Add Vue and Inertia to the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+// import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp, Link, Head } from '@inertiajs/vue3';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+// import Layout from './Layouts/Layout.vue';
+
+
+createInertiaApp({
+    title: (title) => `Test Store | ${title}`,
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+        const page = pages[ `./Pages/${name}.vue` ];
+
+        // page.default.layout = page.default.layout ?? Layout;
+
+        return page;
+    },
+    setup ({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .component('Link', Link)
+            .component('Head', Head)
+            .mount(el);
+    }
+});
